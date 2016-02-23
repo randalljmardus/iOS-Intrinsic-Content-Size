b0a745121713022d7fb47d0155b2473b43e674f7
Randall Mardus <randalljmardus@gmail.com>
2016-02-23 11:22:41 -0500

    first commit

--- iOS Intrinsic Content Size/ViewController.swift
+++ iOS Intrinsic Content Size/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    let nameLabel = UILabel()
+        nameLabel.text = "UILabel"
+        
+        let red = UIColor(hue: 358/360, saturation: 0.6, brightness: 0.98, alpha: 1.0)
+        
+        nameLabel.backgroundColor = red
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(nameLabel)
+        
+        nameLabel.topAnchor.constraintEqualToAnchor(view.topAnchor, constant: 40).active = true
+        nameLabel.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor, constant: 10).active = true
+        
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
